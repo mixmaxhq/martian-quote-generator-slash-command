52678b236d5497e4015476bcf0bcc0c90e8865ef
Brad Vogel <brad@mixmax.com>
2015-10-28 12:18:51 -0700

    CDN image

--- api/resolver.js
+++ api/resolver.js
@@ -5,7 +5,7 @@ var _ = require('underscore');
 module.exports = function(req, res) {
   var term = req.query.text.trim();
 
-  var html = '<p><img style="max-width:100%; box-sizing:border-box;" src="https://dl.dropboxusercontent.com/u/14168496/martian_inboxlove.png"/></p>' +
+  var html = '<p><img style="max-width:100%; box-sizing:border-box;" src="http://mixmax-res.cloudinary.com/image/upload/v1446059883/martian_inboxlove_dmxjou.png"/></p>' +
     '<h4 style="margin: 5px;">' +
     '<font face="Baskerville, Baskerville Old Face, Hoefler Text, Garamond, Times New Roman, serif">“' + term + '”</font>' +
     '</h4>' +
