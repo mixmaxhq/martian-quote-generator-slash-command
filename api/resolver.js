2bcca9e88bfe1e374920020bf483f6104577e0f9
Brad Vogel <brad@mixmax.com>
2015-10-28 12:13:41 -0700

    Add resolver.

--- api/resolver.js
+++ api/resolver.js
@@ -8,7 +8,8 @@ module.exports = function(req, res) {
   var html = '<p><img style="max-width:100%; box-sizing:border-box;" src="https://dl.dropboxusercontent.com/u/14168496/martian_inboxlove.png"/></p>' +
     '<h4 style="margin: 5px;">' +
     '<font face="Baskerville, Baskerville Old Face, Hoefler Text, Garamond, Times New Roman, serif">“' + term + '”</font>' +
-    '</h4>';
+    '</h4>' +
+    '<p style="max-width: 590px; width: 100%;"><img height="1" title="separator" alt="separator" width="600" style="width:100%; height: 1px; max-height:1px; vertical-align:middle;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAlgAAAACCAYAAACE7KJkAAAAI0lEQVRYR+3DQQkAAAgEsOvPYRFLWsLnBksAAPjV7qiq+vcAAMtuJOjWUFsAAAAASUVORK5CYII="></p>';
 
   res.json({
     body: html
