3c7833ce0b78de144a92b8fd69b9af57d0726c26
Brad Vogel <brad@mixmax.com>
2015-10-28 12:12:27 -0700

    Styling fixes.

--- api/resolver.js
+++ api/resolver.js
@@ -5,12 +5,10 @@ var _ = require('underscore');
 module.exports = function(req, res) {
   var term = req.query.text.trim();
 
-  var html = '<h4 style="margin: 5px;">' +
-    '<font color="#999999" size="5">“</font>' +
-    '<font face="Baskerville, Baskerville Old Face, Hoefler Text, Garamond, Times New Roman, serif">' + term + '</font>' +
-    '<font color="#999999" size="5">”</font>' +
-    '</h4>' +
-    '<p><img style="max-width:100%; box-sizing:border-box;" src="https://dl.dropboxusercontent.com/u/14168496/martian_inboxlove.png"/></p>';
+  var html = '<p><img style="max-width:100%; box-sizing:border-box;" src="https://dl.dropboxusercontent.com/u/14168496/martian_inboxlove.png"/></p>' +
+    '<h4 style="margin: 5px;">' +
+    '<font face="Baskerville, Baskerville Old Face, Hoefler Text, Garamond, Times New Roman, serif">“' + term + '”</font>' +
+    '</h4>';
 
   res.json({
     body: html
